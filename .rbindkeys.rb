e5dbc3dae4714075e0161c7cd9aac45c3e6c690c
Yuki Ito <acomagu@gmail.com>
2016-11-21 16:09:33 +0900

    most initial commit

--- .rbindkeys.rb
+++ .rbindkeys.rb
@@ -4,7 +4,49 @@ require 'thread'
 require 'socket'
 
 pre_bind_key KEY_CAPSLOCK, KEY_LEFTCTRL
+pre_bind_key KEY_RO, KEY_RIGHTSHIFT
 bind_key [KEY_LEFTCTRL, KEY_J], KEY_BACKSPACE
+
+def dvorak_to_qwerty keys
+  def convert_key key
+    table = {
+      KEY_A => KEY_A,
+      KEY_B => KEY_X,
+      KEY_C => KEY_J,
+      KEY_D => KEY_E,
+      KEY_E => KEY_DOT,
+      KEY_F => KEY_U,
+      KEY_G => KEY_I,
+      KEY_H => KEY_D,
+      KEY_I => KEY_C,
+      KEY_J => KEY_H,
+      KEY_K => KEY_T,
+      KEY_L => KEY_N,
+      KEY_M => KEY_M,
+      KEY_N => KEY_B,
+      KEY_O => KEY_R,
+      KEY_P => KEY_L,
+      KEY_Q => KEY_APOSTROPHE,
+      KEY_R => KEY_P,
+      KEY_S => KEY_O,
+      KEY_T => KEY_Y,
+      KEY_U => KEY_G,
+      KEY_V => KEY_K,
+      KEY_W => KEY_COMMA,
+      KEY_X => KEY_Q,
+      KEY_Y => KEY_F,
+      KEY_Z => KEY_SEMICOLON,
+      KEY_DOT => KEY_V,
+      KEY_COMMA => KEY_W,
+      KEY_SEMICOLON => KEY_S,
+      KEY_SLASH => KEY_Z,
+      KEY_APOSTROPHE => KEY_MINUS
+    }
+    table.invert()[key]
+  end
+
+  (keys.instance_of? Array) ? keys.map{|k| convert_key(k)} : convert_key(keys)
+end
 
 def push_key key
   operator.press_key key
@@ -26,13 +68,14 @@ end
 def using_ime?
   status = false;
   UNIXSocket.open('/tmp/fcitx-socket-:0') do |sock|
-    sock.send_io 0
+    sock.write [0].pack('i!')
     status = sock.readline.chomp.unpack('i!')[0] == 2
   end
   status
 end
 
 def bind_ime_with_thumb tkey, lkey, rkey
+  # rkey = dvorak_to_qwerty(rkey)
   bind_ime [tkey, lkey], rkey
   bind_ime [lkey, tkey], rkey do |event, operator|
     operator.press_key KEY_BACKSPACE
@@ -40,20 +83,42 @@ def bind_ime_with_thumb tkey, lkey, rkey
   end
 end
 
-def bind_ime lval, rval, defrval=nil
+def bind_ime lval, rval
+  rval = dvorak_to_qwerty(rval)
   bind_key lval do |event, operator|
     yield event, operator if block_given?
-    push_keys (using_ime? ? rval : (defrval.nil? ? lval : defrval))
+    push_keys (using_ime? ? rval : lval)
   end
 end
 
 bind_ime KEY_MUHENKAN, []
 bind_ime KEY_HENKAN, []
 
-bind_key [KEY_LEFTCTRL, KEY_LEFTALT, KEY_J], [KEY_LEFTCTRL, KEY_LEFTALT, KEY_LEFT]
-bind_key [KEY_LEFTCTRL, KEY_LEFTALT, KEY_C], [KEY_LEFTCTRL, KEY_LEFTALT, KEY_DOWN]
-bind_key [KEY_LEFTCTRL, KEY_LEFTALT, KEY_V], [KEY_LEFTCTRL, KEY_LEFTALT, KEY_UP]
-bind_key [KEY_LEFTCTRL, KEY_LEFTALT, KEY_P], [KEY_LEFTCTRL, KEY_LEFTALT, KEY_RIGHT]
+# bind_key [KEY_LEFTCTRL, KEY_LEFTALT, KEY_J], [KEY_LEFTCTRL, KEY_LEFTALT, KEY_LEFT]
+# bind_key [KEY_LEFTCTRL, KEY_LEFTALT, KEY_C], [KEY_LEFTCTRL, KEY_LEFTALT, KEY_DOWN]
+# bind_key [KEY_LEFTCTRL, KEY_LEFTALT, KEY_V], [KEY_LEFTCTRL, KEY_LEFTALT, KEY_UP]
+# bind_key [KEY_LEFTCTRL, KEY_LEFTALT, KEY_P], [KEY_LEFTCTRL, KEY_LEFTALT, KEY_RIGHT]
+
+# bind_key [KEY_LEFTSHIFT, KEY_LEFTALT, KEY_V], KEY_UP
+# bind_key [KEY_LEFTSHIFT, KEY_LEFTALT, KEY_P], KEY_RIGHT
+# bind_key [KEY_LEFTSHIFT, KEY_LEFTALT, KEY_C], KEY_DOWN
+# bind_key [KEY_LEFTSHIFT, KEY_LEFTALT, KEY_J], KEY_LEFT
+# bind_key [KEY_LEFTSHIFT, KEY_LEFTALT, KEY_R], KEY_UP
+# bind_key [KEY_LEFTSHIFT, KEY_LEFTALT, KEY_Y], KEY_RIGHT
+# bind_key [KEY_LEFTSHIFT, KEY_LEFTALT, KEY_L], KEY_DOWN
+# bind_key [KEY_LEFTSHIFT, KEY_LEFTALT, KEY_N], KEY_LEFT
+
+bind_key [KEY_KATAKANAHIRAGANA, KEY_V], KEY_UP
+bind_key [KEY_KATAKANAHIRAGANA, KEY_P], KEY_RIGHT
+bind_key [KEY_KATAKANAHIRAGANA, KEY_C], KEY_DOWN
+bind_key [KEY_KATAKANAHIRAGANA, KEY_J], KEY_LEFT
+bind_key [KEY_KATAKANAHIRAGANA, KEY_R], KEY_UP
+bind_key [KEY_KATAKANAHIRAGANA, KEY_Y], KEY_RIGHT
+bind_key [KEY_KATAKANAHIRAGANA, KEY_L], KEY_DOWN
+bind_key [KEY_KATAKANAHIRAGANA, KEY_N], KEY_LEFT
+
+bind_key [KEY_KATAKANAHIRAGANA, KEY_H], KEY_PAGEDOWN
+bind_key [KEY_KATAKANAHIRAGANA, KEY_F], KEY_PAGEUP
 
 # Single Key Input
 bind_ime KEY_A, KEY_U
